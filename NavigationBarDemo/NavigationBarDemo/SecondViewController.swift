1f8be0c1b8b40fb04298e23cb06d352cafa09d1b
ShinChven <shinchven@gmail.com>
2015-05-03 18:10:57 +0800

    more modifications

--- NavigationBarDemo/NavigationBarDemo/SecondViewController.swift
+++ NavigationBarDemo/NavigationBarDemo/SecondViewController.swift
@@ -9,28 +9,39 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.navigationItem.title = "Second View"
+        
         // Do any additional setup after loading the view.
     }
-
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        // modify your navigationbar in single view here
+        self.navigationItem.title = "Second View"
+        
+        // modify navigationbar style in single view
+        // if you modify uinavigationbar here, it will affect UINavigationBar in all viewcontroller. Because they all use the same UINavigationBar.
+        // However the navigation bar title will not be affect in modification here.
+        self.navigationController?.navigationBar.barTintColor = UIColor.orangeColor()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
-
+    
 }

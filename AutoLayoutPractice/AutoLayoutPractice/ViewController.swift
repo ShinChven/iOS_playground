75aefa4d46316b49f02001183bdf76c9c68e3cd1
ShinChven <shinchven@gmail.com>
2015-05-14 00:15:50 +0800

    AutoLayout Practice

---
+++ AutoLayoutPractice/AutoLayoutPractice/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  AutoLayoutPractice
+//
+//  Created by ShinChven Zhang on 15/5/13.
+//  Copyright (c) 2015年 ShinChven Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 15
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell:UITableViewCell = tableView.dequeueReusableCellWithIdentifier("m_cell", forIndexPath: indexPath) as! UITableViewCell
+        return cell
+    }
+
+
+}
+
